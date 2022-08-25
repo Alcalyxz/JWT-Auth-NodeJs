54abe2bbe4c51c20bb9b36ef4dbff332227a440a
Alcalyxz <jposadasepulveda@gmail.com>
2022-08-25 16:44:13 -0500

    Finish Course

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,8 +2,13 @@ import { Router } from "express";
 const router = Router();
 
 import * as authCtrl from "../controllers/auth.controller";
+import { verifySignUp } from "../middlewars";
 
 router.post("/signin", authCtrl.signin);
-router.post("/signup", authCtrl.signup);
+router.post(
+  "/signup",
+  [verifySignUp.checkDuplicateUsernameOrEmail, verifySignUp.checkRolesExisted],
+  authCtrl.signup
+);
 
 export default router;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,13 @@
-import { Router} from "express";
+import { Router } from "express";
 const router = Router();
+
+import * as userCtrl from "../controllers/user.controller";
+import { authJwt, verifySignUp } from "../middlewars";
+
+router.post(
+  "/",
+  [authJwt.verifyToken, authJwt.isAdmin, verifySignUp.checkRolesExisted],
+  userCtrl.createUser
+);
 
 export default router;
